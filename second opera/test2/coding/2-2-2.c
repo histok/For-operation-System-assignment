737c97cf4578a5853bc16797786889d41ada7949
Eureka <114811932+histok@users.noreply.github.com>
2023-11-14 16:43:16 +0800

    Update 2-2-2.c

--- second opera/test2/coding/2-2-2.c
+++ second opera/test2/coding/2-2-2.c
@@ -10,24 +10,24 @@ int main(){
 	pipe(fd);
 	while((pid1=fork())==-1);
 	if(pid1==0){
-		lockf(fd[1],1,0);
+		//lockf(fd[1],1,0);
 		for(int i=0;i<2000;i++)
 		{	
 			write(fd[1],&c1,1);
 		}
 		sleep(5);
-		lockf(fd[0],0,0);
+		//lockf(fd[0],0,0);
 		exit(0);
 	}
 	else{
 		while((pid2 =fork())==-1);
 		if(pid2==0){
-			lockf(fd[0],1,0);
+			//lockf(fd[0],1,0);
 			for(int i=0;i<2000;i++){
 				write(fd[1],&c2,1);
 			}
 			sleep(5);
-			lockf(fd[0],0,0);
+			//lockf(fd[0],0,0);
 			exit(0);
 		}
 		else{
